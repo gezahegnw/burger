8f95fe761a657a2977d4258a546f44a2fe37f2d4
gezahegnw <gezahegnw@gmail.com>
2018-11-13 14:48:57 -0600

    first push

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+### Populate data with 4 starter burgers
+INSERT INTO burgers (burger_name) VALUES ('Whiskey King Burger.');
+
+INSERT INTO burgers (burger_name) VALUES ('Double Animal Style');
+
+INSERT INTO burgers (burger_name) VALUES ('The Original Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Dyer’s Deep-Fried Burger');
+
