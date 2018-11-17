d541625a8d142b94ac28f3cb074d009b26613cb3
gezahegnw <gezahegnw@gmail.com>
2018-11-17 13:32:43 -0600

    last push

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
 // Set up MySQL connection.
+//mysql://ifksh9qrcmlncesr:f4976jkqq1cjzllr@o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/rcyrcfoln35b6xbc
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "mysql://o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
+  user: "ifksh9qrcmlncesr",
+  password: "f4976jkqq1cjzllr",
+  database: "rcyrcfoln35b6xbc"
 });
 
 // Make connection.
