57afb372fc01d68ef1c9a5396775ccdc055ad99d
gezahegnw <gezahegnw@gmail.com>
2018-11-17 19:11:49 -0600

    protected the database password

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,10 @@
 // Set up MySQL connection.
-//mysql://ifksh9qrcmlncesr:f4976jkqq1cjzllr@o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/rcyrcfoln35b6xbc
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
   user: "ifksh9qrcmlncesr",
- // password: "f4976jkqq1cjzllr",
  password: process.env.DB_PASSWORD,
   database: "rcyrcfoln35b6xbc"
 });
